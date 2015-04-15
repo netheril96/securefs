9327b5651d2c737d668db4adc2a481c405cba430
Siyuan Ren <netheril96@gmail.com>
2015-04-15 11:30:06 +0800

    Implementation of basic file classes

--- sources/files.cpp
+++ sources/files.cpp
@@ -1,8 +1,12 @@
 #include "files.h"
 
+#include <unordered_map>
+#include <utility>
+#include <algorithm>
+
 namespace securefs
 {
-void Filebase::read_header()
+void FileBase::read_header()
 {
     byte header[sizeof(m_mode) + sizeof(m_uid) + sizeof(m_gid) + sizeof(m_root_page)
                 + sizeof(m_start_free_page) + sizeof(m_num_free_page)];
@@ -32,5 +36,121 @@ void Filebase::read_header()
         m_num_free_page = from_little_endian<decltype(m_num_free_page)>(ptr);
         ptr += sizeof(m_num_free_page);
     }
+}
+
+FileBase::~FileBase() {}
+
+void FileBase::flush()
+{
+    if (m_dirty)
+    {
+        byte header[sizeof(m_mode) + sizeof(m_uid) + sizeof(m_gid) + sizeof(m_root_page)
+                    + sizeof(m_start_free_page) + sizeof(m_num_free_page)];
+
+        m_dirty = false;
+    }
+}
+
+namespace internal
+{
+    class SimpleDirectory : public Directory
+    {
+    private:
+        std::unordered_map<std::string, std::pair<id_type, int>> m_table;
+        bool m_dirty;
+
+    public:
+        explicit SimpleDirectory(std::shared_ptr<StreamBase> stream,
+                                 std::shared_ptr<HeaderBase> header)
+            : Directory(std::move(stream), std::move(header)), m_dirty(false)
+        {
+            char buffer[Directory::MAX_FILENAME_LENGTH + 1 + 32 + 4];
+            offset_type off = 0;
+            std::string name;
+            std::pair<id_type, int> value;
+            while (true)
+            {
+                auto rv = this->m_stream->read(buffer, off, sizeof(buffer));
+                if (rv < sizeof(buffer))
+                    break;
+                buffer[MAX_FILENAME_LENGTH]
+                    = 0;    // Set the null terminator in case the data is corrupted
+                name = buffer;
+                memcpy(value.first.data(), buffer + Directory::MAX_FILENAME_LENGTH + 1, ID_LENGTH);
+                value.second
+                    = from_little_endian<uint32_t>(buffer + sizeof(buffer) - sizeof(uint32_t));
+                m_table.emplace(std::move(name), std::move(value));
+                off += sizeof(buffer);
+            }
+        }
+
+        bool get_entry(const std::string& name, id_type& id, int& type) override
+        {
+            auto it = m_table.find(name);
+            if (it == m_table.end())
+                return false;
+            memcpy(id.data(), it->second.first.data(), id.size());
+            type = it->second.second;
+            return true;
+        }
+
+        bool add_entry(const std::string& name, const id_type& id, int type) override
+        {
+            if (name.size() > MAX_FILENAME_LENGTH)
+                throw OSException(ENAMETOOLONG);
+            auto rv = m_table.emplace(name, std::make_pair(id, type));
+            if (rv.second)
+                m_dirty = true;
+            return rv.second;
+        }
+
+        bool remove_entry(const std::string& name) override
+        {
+            auto count = m_table.erase(name);
+            if (count > 0)
+                m_dirty = true;
+            return count > 0;
+        }
+
+        void subflush() override
+        {
+            if (m_dirty)
+            {
+                char buffer[Directory::MAX_FILENAME_LENGTH + 1 + 32 + 4];
+                offset_type off = 0;
+                for (auto&& pair : m_table)
+                {
+                    memset(buffer, 0, sizeof(buffer));
+                    if (pair.first.size() > MAX_FILENAME_LENGTH)
+                        continue;
+                    memcpy(buffer, pair.first.data(), pair.first.size());
+                    memcpy(buffer + MAX_FILENAME_LENGTH + 1, pair.second.first.data(), ID_LENGTH);
+                    to_little_endian(static_cast<uint32_t>(pair.second.second),
+                                     buffer + sizeof(buffer) - sizeof(uint32_t));
+                    this->m_stream->write(buffer, off, sizeof(buffer));
+                    off += sizeof(buffer);
+                }
+                m_dirty = false;
+            }
+        }
+
+        ~SimpleDirectory()
+        {
+            try
+            {
+                flush();
+            }
+            catch (...)
+            {
+                // Ignore exceptions in destructor
+            }
+        }
+    };
+}
+
+std::shared_ptr<Directory> make_directory(std::shared_ptr<StreamBase> stream,
+                                          std::shared_ptr<HeaderBase> header)
+{
+    return std::make_shared<internal::SimpleDirectory>(std::move(stream), std::move(header));
 }
 }
--- sources/files.h
+++ sources/files.h
@@ -8,12 +8,11 @@
 
 namespace securefs
 {
-class Filebase
+class FileBase
 {
 private:
     std::mutex m_lock;
     std::shared_ptr<HeaderBase> m_header;
-    std::shared_ptr<StreamBase> m_stream;
     uint32_t m_mode, m_uid, m_gid;
     uint32_t m_root_page, m_start_free_page, m_num_free_page;
     bool m_dirty;
@@ -22,6 +21,8 @@ private:
     void read_header();
 
 protected:
+    std::shared_ptr<StreamBase> m_stream;
+
     uint32_t get_root_page() const noexcept { return m_root_page; }
     void set_root_page(uint32_t value) noexcept
     {
@@ -50,12 +51,14 @@ public:
     static const int REGULAR_FILE = 0, SYMLINK = 1, DIRECTORY = 2;
 
 public:
-    explicit Filebase(std::shared_ptr<StreamBase> stream, std::shared_ptr<HeaderBase> header)
+    explicit FileBase(std::shared_ptr<StreamBase> stream, std::shared_ptr<HeaderBase> header)
         : m_stream(stream), m_header(header)
     {
+        if (!m_stream || !m_header)
+            NULL_EXCEPT();
         read_header();
     }
-    virtual ~Filebase() {}
+    virtual ~FileBase();
     uint32_t get_mode() const noexcept { return m_mode; }
     void set_mode(uint32_t value) noexcept
     {
@@ -74,9 +77,66 @@ public:
         m_gid = value;
         m_dirty = true;
     }
-    DISABLE_COPY_MOVE(Filebase);
+    DISABLE_COPY_MOVE(FileBase);
 
     virtual int type() const noexcept = 0;
     void flush();
 };
+
+class RegularFile : public FileBase
+{
+public:
+    explicit RegularFile(std::shared_ptr<StreamBase> stream, std::shared_ptr<HeaderBase> header)
+        : FileBase(std::move(stream), std::move(header))
+    {
+    }
+    int type() const noexcept override { return FileBase::REGULAR_FILE; }
+    length_type read(void* output, offset_type off, length_type len)
+    {
+        return this->m_stream->read(output, off, len);
+    }
+    void write(const void* input, offset_type off, length_type len)
+    {
+        return this->m_stream->write(input, off, len);
+    }
+    length_type size() const noexcept { return m_stream->size(); }
+    void truncate(length_type new_size) { return m_stream->resize(new_size); }
+};
+
+class Symlink : public FileBase
+{
+public:
+    explicit Symlink(std::shared_ptr<StreamBase> stream, std::shared_ptr<HeaderBase> header)
+        : FileBase(std::move(stream), std::move(header))
+    {
+    }
+    int type() const noexcept override { return FileBase::SYMLINK; }
+    std::string get()
+    {
+        std::string result(m_stream->size(), 0);
+        m_stream->read(&result[0], 0, result.size());
+        return result;
+    }
+    void set(const std::string& path) { m_stream->write(path.data(), 0, path.size()); }
+};
+
+class Directory : public FileBase
+{
+public:
+    static const size_t MAX_FILENAME_LENGTH = 255;
+
+public:
+    explicit Directory(std::shared_ptr<StreamBase> stream, std::shared_ptr<HeaderBase> header)
+        : FileBase(std::move(stream), std::move(header))
+    {
+    }
+    int type() const noexcept override { return FileBase::DIRECTORY; }
+
+    virtual bool get_entry(const std::string& name, id_type& id, int& type) = 0;
+    virtual bool add_entry(const std::string& name, const id_type& id, int type) = 0;
+    virtual bool remove_entry(const std::string& name) = 0;
+};
+
+std::shared_ptr<Directory> make_directory(std::shared_ptr<StreamBase> stream,
+                                          std::shared_ptr<HeaderBase> header);
 }
