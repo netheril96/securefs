8ead48d3b044acebee367980f1598a9f43336416
Siyuan Ren <netheril96@gmail.com>
2015-04-19 12:12:15 +0800

    Reseed the pool after many bytes generated

--- sources/utils.cpp
+++ sources/utils.cpp
@@ -7,6 +7,13 @@ namespace securefs
 void generate_random(void* data, size_t size)
 {
     thread_local CryptoPP::AutoSeededRandomPool pool;
+    thread_local size_t total = 0;
     pool.GenerateBlock(static_cast<byte*>(data), size);
+    total += size;
+    if (total > 1024 * 1024)
+    {
+        total = 0;
+        pool.Reseed();
+    }
 }
 }
