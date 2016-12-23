ef81e70354ca2591b4f7a97f76ef2a914dc26920
Siyuan Ren <netheril96@gmail.com>
2016-12-23 16:33:44 +0800

    Use clock_gettime whenever possible

--- cmake/FeatureDetection.cmake
+++ cmake/FeatureDetection.cmake
@@ -1,15 +1,15 @@
 include(CheckCXXSourceCompiles)
 include(CheckFunctionExists)
 
-set (TMP_FLAGS ${CMAKE_REQUIRED_FLAGS})
+set(TMP_FLAGS ${CMAKE_REQUIRED_FLAGS})
 if (UNIX)
-    set (CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} -std=c++11")
-endif()
+    set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} -std=c++11")
+endif ()
 
 CHECK_CXX_SOURCE_COMPILES("int main() { thread_local int a = 0; return a; }" HAS_THREAD_LOCAL)
 if (${HAS_THREAD_LOCAL})
     add_definitions(-DHAS_THREAD_LOCAL)
-endif()
+endif ()
 set(CMAKE_REQUIRED_FLAGS ${TMP_FLAGS})
 
 CHECK_FUNCTION_EXISTS(openat HAS_OPENAT)
@@ -17,11 +17,16 @@ CHECK_FUNCTION_EXISTS(unlinkat HAS_UNLINKAT)
 CHECK_FUNCTION_EXISTS(mkdirat HAS_MKDIRAT)
 CHECK_FUNCTION_EXISTS(renameat HAS_RENAMEAT)
 CHECK_FUNCTION_EXISTS(futimens HAS_FUTIMENS)
+CHECK_FUNCTION_EXISTS(clock_gettime HAS_CLOCK_GETTIME)
 
 if (${HAS_OPENAT} AND ${HAS_UNLINKAT} AND ${HAS_MKDIRAT} AND ${HAS_RENAMEAT})
     add_definitions(-DHAS_AT_FUNCTIONS)
-endif()
+endif ()
 
 if (HAS_FUTIMENS)
     add_definitions(-DHAS_FUTIMENS)
-endif()
+endif ()
+
+if (HAS_CLOCK_GETTIME)
+    add_definitions(-DHAS_CLOCK_GETTIME)
+endif ()
--- sources/unix.cpp
+++ sources/unix.cpp
@@ -15,10 +15,9 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <time.h>
 
 #ifdef __APPLE__
-#include <mach/clock.h>
-#include <mach/mach.h>
 #include <sys/xattr.h>
 #endif
 
@@ -295,16 +294,13 @@ bool OSService::isatty(int fd) noexcept { return ::isatty(fd) != 0; }
 
 void OSService::get_current_time(timespec& current_time)
 {
-#ifdef __MACH__    // OS X does not have clock_gettime, use clock_get_time
-    clock_serv_t cclock;
-    mach_timespec_t mts;
-    host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-    clock_get_time(cclock, &mts);
-    mach_port_deallocate(mach_task_self(), cclock);
-    current_time.tv_sec = mts.tv_sec;
-    current_time.tv_nsec = mts.tv_nsec;
-#else
+#ifdef HAS_CLOCK_GETTIME
     clock_gettime(CLOCK_REALTIME, &current_time);
+#else
+    timeval tv;
+    gettimeofday(&tv, nullptr);
+    current_time.tv_sec = tv.tv_sec;
+    current_time.tv_nsec = tv.tv_usec * 1000;
 #endif
 }
 }
