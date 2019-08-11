09c039be9027a135bb0e54302847bd6b93a0bc89
Siyaun Ren <rensiyuan@google.com>
2019-08-11 13:55:01 +0800

    Implements `transform_listxattr_result`

--- sources/apple_xattr_workaround.cpp
+++ sources/apple_xattr_workaround.cpp
@@ -2,9 +2,36 @@
 
 #ifdef __APPLE__
 
+#include <string.h>
+
 namespace securefs
 {
-void transform_listxattr_result(char* buffer, size_t size);
+static const char APPLE_FINDER_INFO[] = "com.apple.FinderInfo";
+static const char REPLACEMENT_FOR_FINDER_INFO[] = "_securefs.FinderInfo";
+static const char APPLE_QUARANTINE[] = "com.apple.quarantine";
+
+static_assert(sizeof(APPLE_FINDER_INFO) == sizeof(REPLACEMENT_FOR_FINDER_INFO),
+              "The two \"FinderInfo\" attribute names must have the same size");
+
+void transform_listxattr_result(char* buffer, size_t size)
+{
+    if (size < sizeof(APPLE_FINDER_INFO))
+        return;
+
+    for (size_t i = 0; i <= size - sizeof(APPLE_FINDER_INFO); ++i)
+    {
+        if (i > 0 && buffer[i - 1] != '\0')
+        {
+            continue;    // Not a string boundary.
+        }
+        // The terminating null must be compared too.
+        if (memcmp(buffer + i, REPLACEMENT_FOR_FINDER_INFO, sizeof(REPLACEMENT_FOR_FINDER_INFO))
+            == 0)
+        {
+            memcpy(buffer + i, APPLE_FINDER_INFO, sizeof(APPLE_FINDER_INFO));
+        }
+    }
+}
 }
 
 #endif
