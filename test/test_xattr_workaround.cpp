0fa14aef2ba3b0d7423031ec7074f2e0c0efe8ba
Siyaun Ren <rensiyuan@google.com>
2019-08-11 14:26:04 +0800

    More tests.

--- test/test_xattr_workaround.cpp
+++ test/test_xattr_workaround.cpp
@@ -12,6 +12,24 @@ static std::string to_string(const char (&buffer)[N])
 TEST_CASE("transform listxattr result")
 {
     {
+        char buffer[] = "";
+        const char canonical_result[] = "";
+        securefs::transform_listxattr_result(buffer, sizeof(buffer));
+        REQUIRE(to_string(buffer) == to_string(canonical_result));
+    }
+    {
+        char buffer[] = "abc";
+        const char canonical_result[] = "abc";
+        securefs::transform_listxattr_result(buffer, sizeof(buffer));
+        REQUIRE(to_string(buffer) == to_string(canonical_result));
+    }
+    {
+        char buffer[] = "a";
+        const char canonical_result[] = "a";
+        securefs::transform_listxattr_result(buffer, sizeof(buffer));
+        REQUIRE(to_string(buffer) == to_string(canonical_result));
+    }
+    {
         char buffer[] = "com.apple.FinderInfo";
         const char canonical_result[] = "com.apple.FinderInfo";
         securefs::transform_listxattr_result(buffer, sizeof(buffer));
@@ -24,27 +42,39 @@ TEST_CASE("transform listxattr result")
         REQUIRE(to_string(buffer) == to_string(canonical_result));
     }
     {
-        char buffer[] = "_securefs.FinderInfo\0abcde";
+        char buffer[] = "_securefs.FinderInf";
+        const char canonical_result[] = "_securefs.FinderInf";
         securefs::transform_listxattr_result(buffer, sizeof(buffer));
+        REQUIRE(to_string(buffer) == to_string(canonical_result));
+    }
+    {
+        char buffer[] = "_securefs.FinderInfoa";
+        const char canonical_result[] = "_securefs.FinderInfoa";
+        securefs::transform_listxattr_result(buffer, sizeof(buffer));
+        REQUIRE(to_string(buffer) == to_string(canonical_result));
+    }
+    {
+        char buffer[] = "_securefs.FinderInfo\0abcde";
         const char canonical_result[] = "com.apple.FinderInfo\0abcde";
+        securefs::transform_listxattr_result(buffer, sizeof(buffer));
         REQUIRE(to_string(buffer) == to_string(canonical_result));
     }
     {
         char buffer[] = "abcde\0_securefs.FinderInfo";
-        securefs::transform_listxattr_result(buffer, sizeof(buffer));
         const char canonical_result[] = "abcde\0com.apple.FinderInfo";
+        securefs::transform_listxattr_result(buffer, sizeof(buffer));
         REQUIRE(to_string(buffer) == to_string(canonical_result));
     }
     {
         char buffer[] = "abcdefghijklmnopkrstuvwxyz\0_securefs.FinderInfo";
-        securefs::transform_listxattr_result(buffer, sizeof(buffer));
         const char canonical_result[] = "abcdefghijklmnopkrstuvwxyz\0com.apple.FinderInfo";
+        securefs::transform_listxattr_result(buffer, sizeof(buffer));
         REQUIRE(to_string(buffer) == to_string(canonical_result));
     }
     {
         char buffer[] = "abcde\0_securefs.FinderInfo\0fgh";
-        securefs::transform_listxattr_result(buffer, sizeof(buffer));
         const char canonical_result[] = "abcde\0com.apple.FinderInfo\0fgh";
+        securefs::transform_listxattr_result(buffer, sizeof(buffer));
         REQUIRE(to_string(buffer) == to_string(canonical_result));
     }
 }
