8e005d775c3bc9b816ca5f54bf0fc659af811131
Siyaun Ren <netheril96@gmail.com>
2020-01-01 15:35:11 +0800

    In linux-build.sh, store temporary data on memory.
    Reduce disk I/O.

--- linux-build.sh
+++ linux-build.sh
@@ -2,8 +2,8 @@
 set -x;
 SOURCE_DIR=$(dirname $(realpath "$0"));
 cd "${SOURCE_DIR}" && git submodule update --init;
-OUR_TMP_DIR=$(mktemp -d -t securefs-build-XXXXX)
-cd "${OUR_TMP_DIR}";
+export TMPDIR=/dev/shm;
+cd $(mktemp -d -t securefs-build-XXXXX);
 cmake "${SOURCE_DIR}";
 make -j4;
 SECUREFS_BINARY_PATH=$(realpath ./securefs);
