6652df8c25b7b1db0da3cb4586d1cf2a057d4256
Siyuan Ren <netheril96@gmail.com>
2021-06-13 20:23:05 +0800

    Update to Crypto++ 8.5

--- external/cryptopp/TestPrograms/test_newlib.cxx
+++
@@ -1,8 +0,0 @@
-#include <string>
-int main(int argc, char* argv[])
-{
-#ifndef __NEWLIB__
-	XXX
-#endif
-	return 0;
-}
--- external/cryptopp/TestPrograms/test_ppc_power8.cxx
+++
@@ -1,7 +0,0 @@
-#include <altivec.h>
-int main(int argc, char* argv[])
-{
-	__vector unsigned long long z = {1, 2};
-	z=vec_add(z,z);
-	return 0;
-}
