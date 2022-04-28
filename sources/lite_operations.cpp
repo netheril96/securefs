db1d5e1022c72559fbad348e9b4ac526aee896d7
Siyuan Ren <netheril96@gmail.com>
2022-04-28 20:15:51 +0800

    Revert "Add test workarounds for detecting mount/unmount."
    This reverts commit 2991196deba5a72172de6a3a5323216e13de4320.

--- sources/lite_operations.cpp
+++ sources/lite_operations.cpp
@@ -7,7 +7,6 @@
 #include "myutils.h"
 #include "operations.h"
 #include "platform.h"
-#include "test_workaround.h"
 
 #include <math.h>
 
@@ -99,7 +98,6 @@ namespace lite
         {
             if (!filesystem->stat(path, st))
                 return -ENOENT;
-            TestWorkaround::instance().postprocess_getattr(path, st);
             TRACE_LOG("stat (%s): mode=0%o, uid=%u, gid=%u, size=%zu",
                       path,
                       st->st_mode,
@@ -427,10 +425,6 @@ namespace lite
         SINGLE_COMMON_PROLOGUE
         try
         {
-            if (TestWorkaround::instance().preprocess_utimens(path, ts))
-            {
-                return 0;
-            }
             filesystem->utimens(path, ts);
             return 0;
         }
