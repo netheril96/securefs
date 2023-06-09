3d63c195ca6ce97c7af9e50da7658807e1ef60db
Siyuan Ren <netheril96@gmail.com>
2023-06-09 11:30:32 +0800

    Add build script

---
+++ build.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+import subprocess
+import os
+import tempfile
+import argparse
+
+
+def check_call(*args):
+    print("Executing", args)
+    subprocess.check_call(args)
+
+
+def get_build_root(build_root: str) -> str:
+    if build_root:
+        os.makedirs(build_root, exist_ok=True)
+        return build_root
+    base_tmp_dir = None
+    if os.path.isdir("/dev/shm"):
+        base_tmp_dir = "/dev/shm"
+    return tempfile.mkdtemp(prefix="securefs", dir=base_tmp_dir)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--vcpkg_root",
+        help="The root of vcpkg repository",
+        default=os.path.join(os.environ.get("HOME", "/"), "vcpkg"),
+    )
+    parser.add_argument(
+        "--build_root",
+        help="The root of build directory. If unset, a temporary directory will be used.",
+        default="",
+    )
+    parser.add_argument(
+        "--enable_test",
+        default=False,
+        help="Run test after building to ensure correctness",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--triplet",
+        default="" if os.name != "nt" else "x64-windows-static-md",
+        help="Override the default vcpkg triplet",
+    )
+    args = parser.parse_args()
+    if not os.path.isdir(args.vcpkg_root):
+        raise ValueError(
+            "--vcpkg_root must point to a directory. Install from https://vcpkg.io if necessary."
+        )
+
+    source_dir = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(get_build_root(args.build_root))
+    configure_args = [
+        "cmake",
+        "-DCMAKE_BUILD_TYPE=Release",
+        f"-DCMAKE_TOOLCHAIN_FILE={args.vcpkg_root}/scripts/buildsystems/vcpkg.cmake",
+        source_dir,
+    ]
+    if args.triplet:
+        configure_args.insert(2, "-DVCPKG_TARGET_TRIPLET=" + args.triplet)
+    check_call(*configure_args)
+    check_call("cmake", "--build", ".", "--config", "Release")
+    if args.enable_test:
+        check_call("ctest", "-V", "-C", "Release")
+    print(
+        "Build succeeds. Please copy the binary somewhere in your PATH:",
+        os.path.realpath("./securefs"),
+    )
+
+
+if __name__ == "__main__":
+    main()
