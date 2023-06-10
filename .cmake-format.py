d15d7e235014e650bffb111f8a7588d35e3c6197
Siyuan Ren <netheril96@gmail.com>
2023-06-10 10:23:48 +0800

    End of line conversion

--- .cmake-format.py
+++ .cmake-format.py
@@ -2,7 +2,6 @@
 # Options affecting listfile parsing
 # ----------------------------------
 with section("parse"):
-
     # Specify structure for custom cmake functions
     additional_commands = {
         "foo": {
@@ -24,7 +23,6 @@ with section("parse"):
 # Options affecting formatting.
 # -----------------------------
 with section("format"):
-
     # Disable formatting entirely, making cmake-format a no-op
     disable = False
 
@@ -122,7 +120,6 @@ with section("format"):
 # Options affecting comment reflow and formatting.
 # ------------------------------------------------
 with section("markup"):
-
     # What character to use for bulleted lists
     bullet_char = "*"
 
@@ -166,7 +163,6 @@ with section("markup"):
 # Options affecting the linter
 # ----------------------------
 with section("lint"):
-
     # a list of lint codes to disable
     disabled_codes = []
 
@@ -223,7 +219,6 @@ with section("lint"):
 # Options affecting file encoding
 # -------------------------------
 with section("encode"):
-
     # If true, emit the unicode byte-order mark (BOM) at the start of the file
     emit_byteorder_mark = False
 
@@ -238,7 +233,6 @@ with section("encode"):
 # Miscellaneous configurations options.
 # -------------------------------------
 with section("misc"):
-
     # A dictionary containing any per-command configuration overrides. Currently
     # only `command_case` is supported.
     per_command = {}
