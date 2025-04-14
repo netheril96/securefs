36586348625c17cf23b77bd39aac5643d222ba1c
Siyuan Ren <netheril96@gmail.com>
2025-04-14 18:57:58 +0800

    Install failure signal handler in test main.

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,16 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "platform.h"
+#include <absl/debugging/failure_signal_handler.h>
+#include <absl/debugging/symbolize.h>
 #include <doctest/doctest.h>
 
 int main(int argc, char** argv)
 {
+    absl::InitializeSymbolizer(argv[0]);
+    absl::FailureSignalHandlerOptions options;
+    options.use_alternate_stack = true;
+    absl::InstallFailureSignalHandler(options);
+
 #ifdef _WIN32
     securefs::windows_init();
 #endif
