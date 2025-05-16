024ffd0e01bb625b61c6ab7a4223c3c19a6debfd
Siyuan Ren <netheril96@gmail.com>
2025-05-16 20:11:05 +0800

    Make sure debug mode has address sanitizer on

--- overlay_triplets/x64-freebsd.cmake
+++ overlay_triplets/x64-freebsd.cmake
@@ -2,9 +2,9 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
-set(VCPKG_BUILD_TYPE release)
-
 set(VCPKG_CMAKE_SYSTEM_NAME FreeBSD)
 
 set(VCPKG_C_FLAGS_RELEASE "-flto=thin")
 set(VCPKG_CXX_FLAGS_RELEASE "-flto=thin")
+set(VCPKG_C_FLAGS_DEBUG "-fsanitize=address")
+set(VCPKG_CXX_FLAGS_DEBUG "-fsanitize=address")
