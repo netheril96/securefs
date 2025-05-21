4467353f97a056144af9f83b5f77c393b0bfe30b
Siyuan Ren <netheril96@gmail.com>
2025-05-21 22:27:35 +0800

    Install failure signal handler

--- main.cpp
+++ main.cpp
@@ -2,13 +2,16 @@
 #include "mystring.h"
 #include "myutils.h"
 #include "platform.h"
+
+#include <absl/debugging/failure_signal_handler.h>
+#include <absl/debugging/symbolize.h>
+
 #include <clocale>
 
 #ifdef _WIN32
 
 int wmain(int argc, wchar_t** wargv)
 {
-    ::securefs::windows_init();
     auto str_argv = securefs::make_unique_array<std::string>(argc);
     for (int i = 0; i < argc; ++i)
         str_argv[i] = securefs::narrow_string(wargv[i]);
@@ -17,9 +20,22 @@ int wmain(int argc, wchar_t** wargv)
         argv[i] = str_argv[i].c_str();
     argv[argc] = nullptr;
 
+    absl::InitializeSymbolizer(argv[0]);
+    absl::FailureSignalHandlerOptions options;
+    options.use_alternate_stack = true;
+    absl::InstallFailureSignalHandler(options);
+
+    ::securefs::windows_init();
     return securefs::commands_main(argc, argv.get());
 }
 
 #else
-int main(int argc, char** argv) { return securefs::commands_main(argc, argv); }
+int main(int argc, char** argv)
+{
+    absl::InitializeSymbolizer(argv[0]);
+    absl::FailureSignalHandlerOptions options;
+    options.use_alternate_stack = true;
+    absl::InstallFailureSignalHandler(options);
+    return securefs::commands_main(argc, argv);
+}
 #endif
