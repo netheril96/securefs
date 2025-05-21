50fb25c729715d1cfee69fcda2eb1c7daad200d0
Siyuan Ren <netheril96@gmail.com>
2025-05-21 23:22:34 +0800

    Enable asan selectively

--- overlay_triplets/arm64-osx.cmake
+++ overlay_triplets/arm64-osx.cmake
@@ -2,10 +2,10 @@ set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
+set(VCPKG_BUILD_TYPE release)
+
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES arm64)
 
 set(VCPKG_C_FLAGS_RELEASE "-flto=thin")
 set(VCPKG_CXX_FLAGS_RELEASE "-flto=thin")
-set(VCPKG_C_FLAGS_DEBUG "-fsanitize=address")
-set(VCPKG_CXX_FLAGS_DEBUG "-fsanitize=address")
