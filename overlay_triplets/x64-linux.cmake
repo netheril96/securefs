2f092e1073adc1fa8f611e99ad88e6c716199e1e
Siyuan Ren <netheril96@gmail.com>
2025-05-21 23:22:34 +0800

    Enable asan selectively

--- overlay_triplets/x64-linux.cmake
+++ overlay_triplets/x64-linux.cmake
@@ -1,9 +1,10 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_BUILD_TYPE release)
+
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
 set(VCPKG_C_FLAGS_RELEASE "-flto")
 set(VCPKG_CXX_FLAGS_RELEASE "-flto")
-set(VCPKG_C_FLAGS_DEBUG "-fsanitize=address")
-set(VCPKG_CXX_FLAGS_DEBUG "-fsanitize=address")
