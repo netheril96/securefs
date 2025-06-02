5b42a1f563103ec0b83714613488081008b65e94
Siyuan Ren <netheril96@gmail.com>
2025-06-02 16:35:04 +0800

    Fix x64-linux-asan

--- overlay_triplets/x64-linux-asan.cmake
+++ overlay_triplets/x64-linux-asan.cmake
@@ -4,6 +4,6 @@ set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
-set(VCPKG_C_FLAGS "-fsanitize=address")
-set(VCPKG_CXX_FLAGS "-fsanitize=address")
-set(VCPKG_LINKER_FLAGS "-fsanitize=address")
+set(VCPKG_C_FLAGS_DEBUG "-fsanitize=address")
+set(VCPKG_CXX_FLAGS_DEBUG "-fsanitize=address")
+set(VCPKG_LINKER_FLAGS_DEBUG "-fsanitize=address")
