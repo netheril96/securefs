e6db522eb3a6b5d5a58b358effbcc47db2f88709
Siyuan Ren <netheril96@gmail.com>
2025-08-10 16:00:53 +0800

    Handle overflow in NtReadFile and NtWriteFile

--- sources/win/nt_stream.cpp
+++ sources/win/nt_stream.cpp
@@ -10,6 +10,9 @@ length_type NTStream::read(void* output, offset_type offset, length_type length)
     if (length == 0)
         return 0;
 
+    if (length > std::numeric_limits<ULONG>::max())
+        throwInvalidArgumentException("length > ULONG_MAX");
+
     IO_STATUS_BLOCK io_status_block;
     LARGE_INTEGER li_offset;
     li_offset.QuadPart = offset;
@@ -20,7 +23,7 @@ length_type NTStream::read(void* output, offset_type offset, length_type length)
                              nullptr,
                              &io_status_block,
                              output,
-                             length,
+                             static_cast<ULONG>(length),
                              &li_offset,
                              nullptr);
 
@@ -40,6 +43,9 @@ void NTStream::write(const void* input, offset_type offset, length_type length)
     if (length == 0)
         return;
 
+    if (length > std::numeric_limits<ULONG>::max())
+        throwInvalidArgumentException("length > ULONG_MAX");
+
     IO_STATUS_BLOCK io_status_block;
     LARGE_INTEGER li_offset;
     li_offset.QuadPart = offset;
@@ -50,7 +56,7 @@ void NTStream::write(const void* input, offset_type offset, length_type length)
                               nullptr,
                               &io_status_block,
                               const_cast<void*>(input),
-                              length,
+                              static_cast<ULONG>(length),
                               &li_offset,
                               nullptr));
 
@@ -62,12 +68,12 @@ length_type NTStream::size() const
 {
     IO_STATUS_BLOCK io_status_block;
     FILE_STANDARD_INFORMATION info;
-    NT_CHECK_CALL(NtQueryInformationFile(
-        m_handle.get(),
-        &io_status_block,
-        &info,
-        sizeof(info),
-        static_cast<FILE_INFORMATION_CLASS>(5) /*FileStandardInformation*/));
+    NT_CHECK_CALL(
+        NtQueryInformationFile(m_handle.get(),
+                               &io_status_block,
+                               &info,
+                               sizeof(info),
+                               static_cast<FILE_INFORMATION_CLASS>(5) /*FileStandardInformation*/));
     return info.EndOfFile.QuadPart;
 }
 
@@ -83,12 +89,12 @@ void NTStream::resize(length_type len)
     FILE_END_OF_FILE_INFORMATION info;
     info.EndOfFile.QuadPart = len;
 
-    NT_CHECK_CALL(NtSetInformationFile(
-        m_handle.get(),
-        &io_status_block,
-        &info,
-        sizeof(info),
-        static_cast<FILE_INFORMATION_CLASS>(20) /*FileEndOfFileInformation*/));
+    NT_CHECK_CALL(
+        NtSetInformationFile(m_handle.get(),
+                             &io_status_block,
+                             &info,
+                             sizeof(info),
+                             static_cast<FILE_INFORMATION_CLASS>(20) /*FileEndOfFileInformation*/));
 }
 
 bool NTStream::is_sparse() const noexcept { return true; }
