2b9661005b3613dde9f28fc9e50b4ef703477d24
Siyuan Ren <netheril96@gmail.com>
2025-12-06 18:39:26 +0800

    Interrupt is not errors

--- src/fuse_wrappers/fuse_main.rs
+++ src/fuse_wrappers/fuse_main.rs
@@ -6,10 +6,10 @@ use scopeguard::defer;
 use crate::fuse_wrappers::{
     bindings::{
         self, fuse_args, fuse_cmdline_opts, fuse_loop_cfg_create, fuse_loop_cfg_destroy,
-        fuse_loop_cfg_set_clone_fd, fuse_loop_cfg_set_max_threads,
-        fuse_lowlevel_ops, fuse_opt_free_args, fuse_parse_cmdline, fuse_remove_signal_handlers,
-        fuse_session, fuse_session_destroy, fuse_session_loop_mt, fuse_session_mount,
-        fuse_session_unmount, fuse_set_signal_handlers,
+        fuse_loop_cfg_set_clone_fd, fuse_loop_cfg_set_max_threads, fuse_lowlevel_ops,
+        fuse_opt_free_args, fuse_parse_cmdline, fuse_remove_signal_handlers, fuse_session,
+        fuse_session_destroy, fuse_session_loop_mt, fuse_session_mount, fuse_session_unmount,
+        fuse_set_signal_handlers,
     },
     fuse_low_level_ops::{FuseLowLevelOps, generate_libfuse_low_level_ops},
 };
@@ -103,7 +103,7 @@ pub fn run_fuse_main<T: FuseLowLevelOps>(
     }
 
     let ret = unsafe { fuse_session_loop_mt(*session, *config) };
-    if ret != 0 {
+    if ret != 0 && ret != libc::SIGINT && ret != libc::SIGTERM && ret != libc::SIGHUP {
         return Err(FuseLoopError).with_context(|| format!("fuse_session_loop_mt returns {ret}"));
     }
     log::info!("fuse_session_loop_mt returned {ret}");
