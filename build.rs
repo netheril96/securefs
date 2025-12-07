e57ddb9e518a2c1121d5d2d43bf4a982b78482ed
Siyuan Ren <netheril96@gmail.com>
2025-12-07 22:43:44 +0800

    Remove implicit dep on protoc

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 fn main() {
     println!("cargo:rerun-if-changed=protos");
     protobuf_codegen::Codegen::new()
+        .pure()
         // All inputs and imports from the inputs must reside in `includes` directories.
         .includes(&["protos"])
         // Inputs must reside in some of include paths.
